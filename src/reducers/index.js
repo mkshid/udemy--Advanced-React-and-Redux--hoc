f4d4ea52e4fa33257c3f172c5a513e5540a704b0
M. Hassan Khurshid <mkshid@gmail.com>
2017-07-06 22:05:37 +0200

    Login component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import authenticationReducer from './authentication_reducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+    authenticated: authenticationReducer
 });
 
 export default rootReducer;
